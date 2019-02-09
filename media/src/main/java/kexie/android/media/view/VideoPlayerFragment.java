63b87f79b80f3bd8f26b6d1a9c14937af227d99e
Luke <694014820@qq.com>
2019-02-10 04:20:23 +0800

    update

--- media/src/main/java/kexie/android/media/view/VideoPlayerFragment.java
+++ media/src/main/java/kexie/android/media/view/VideoPlayerFragment.java
@@ -1,28 +1,19 @@
 package kexie.android.media.view;
 
 import android.content.res.Configuration;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.request.target.Target;
-import com.dou361.ijkplayer.widget.PlayStateParams;
-import com.dou361.ijkplayer.widget.PlayerView;
+import com.dl7.player.media.IjkPlayerView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.percentlayout.widget.PercentFrameLayout;
 import kexie.android.media.R;
 import kexie.android.media.databinding.FragmentVideoPlayerBinding;
 import kexie.android.media.entity.MediaInfo;
@@ -39,7 +30,7 @@ public class VideoPlayerFragment extends Fragment
         return fragment;
     }
 
-    private PlayerView player;
+    private IjkPlayerView player;
 
     private FragmentVideoPlayerBinding binding;
 
@@ -56,88 +47,36 @@ public class VideoPlayerFragment extends Fragment
                 R.layout.fragment_video_player,
                 container,
                 false);
-        player = new PlayerView(getActivity(), binding.getRoot())
-                .setScaleType(PlayStateParams.fitparent)
-                .setPlayerBackListener(() -> {
-                    //这里可以简单播放器点击返回键
-                    getActivity().onBackPressed();
-                })
-                .showThumbnail(
-                        ivThumbnail -> Glide.with(getContext())
-                                .load(info.getPath())
-                                .into(ivThumbnail))
-                .seekTo(0)
-                .setTitle(info.getTitle())
-                .setPlaySource(info.getPath())
-                .forbidTouch(false)
-                .hideRotation(true)
-                .hideMenu(true)
-                .setOnlyFullScreen(true)
-                .hideSteam(true)
-                .setNetWorkTypeTie(false)
-                .startPlay();
-        player.setBrightness(50);
+        player = binding.playerView;
+        Glide.with(this)
+                .load(info.getPath())
+                .apply(RequestOptions.fitCenterTransform())
+                .into(player.mPlayerThumb);
+        binding.playerView.init()
+                .setTitle(info.getTitle())    // set title
+                .setVideoPath(info.getPath())
+                .alwaysFullScreen()
+                .setMediaQuality(IjkPlayerView.MEDIA_QUALITY_HIGH)  // set the initial video url
+                .start();   // Start playing
         return binding.getRoot();
     }
 
     @Override
-    public void onStart()
+    public void onPause()
     {
-        super.onStart();
-
+        super.onPause();
+        if (player != null)
+        {
+            player.onPause();
+        }
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    public void onDestroyView()
     {
-        super.onViewCreated(view, savedInstanceState);
-        ViewTreeObserver observer = view.getViewTreeObserver();
-        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener()
-        {
-            @Override
-            public void onGlobalLayout()
-            {
-                MediaInfo info = getArguments().getParcelable("info");
-                Glide.with(getContext()).asBitmap()
-                        .load(info.getPath())
-                        .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE)
-                                .encodeQuality(1))
-                        .listener(new RequestListener<Bitmap>()
-                {
-                    @Override
-                    public boolean onLoadFailed(@Nullable GlideException e,
-                                                Object model,
-                                                Target<Bitmap> target,
-                                                boolean isFirstResource)
-                    {
-                        return false;
-                    }
-
-                    @Override
-                    public boolean onResourceReady(Bitmap bitmap,
-                                                   Object model,
-                                                   Target<Bitmap> target,
-                                                   DataSource dataSource,
-                                                   boolean isFirstResource)
-                    {
-                        float height = binding.videoViewRoot.getMeasuredHeight();
-                        float width = binding.videoViewRoot.getMeasuredWidth();
-                        float videoHeight
-                                = bitmap.getHeight(); // 视频高度
-                        float videoWidth
-                                = bitmap.getWidth(); // 视频宽
-                        PercentFrameLayout.LayoutParams params
-                                = (PercentFrameLayout.LayoutParams) binding
-                                .videoView
-                                .getLayoutParams();
-                        params.getPercentLayoutInfo().widthPercent
-                                = (height * (videoWidth / videoHeight)) / width;
-                        return true;
-                    }
-                }).submit();
-                observer.removeOnGlobalLayoutListener(this);
-            }
-        });
+        super.onDestroyView();
+        player = null;
+        binding = null;
     }
 
     @Override
@@ -161,12 +100,12 @@ public class VideoPlayerFragment extends Fragment
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig)
+    public void onConfigurationChanged(@NonNull Configuration newConfig)
     {
         super.onConfigurationChanged(newConfig);
         if (player != null)
         {
-            player.onConfigurationChanged(newConfig);
+            player.configurationChanged(newConfig);
         }
     }
 }
