978e50b3cacf53fd4711f0713aa32a23704db3b5
Luke <694014820@qq.com>
2019-04-25 14:25:14 +0800

    修复兼容性问题

--- media/src/main/java/org/kexie/android/dng/media/widget/WindowPlayer.java
+++ media/src/main/java/org/kexie/android/dng/media/widget/WindowPlayer.java
@@ -2,11 +2,15 @@ package org.kexie.android.dng.media.widget;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.FrameLayout;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.yhao.floatwindow.FloatWindow;
+import com.yhao.floatwindow.IFloatWindow;
+import com.yhao.floatwindow.MoveType;
 import com.yhao.floatwindow.Screen;
 
 import org.kexie.android.dng.common.app.PR;
@@ -21,12 +25,14 @@ public final class WindowPlayer
         implements View.OnClickListener,
         IjkPlayerView.OnBackListener {
 
+    private DisplayMetrics displayMetrics;
     private IjkPlayerView player;
+    private View root;
 
     public WindowPlayer(IjkPlayerView player) {
         this.player = player;
         player.setOnClickBackListener(this);
-        View root = View.inflate(
+        root = View.inflate(
                 player.getContext(),
                 R.layout.window_float_player,
                 null);
@@ -35,16 +41,37 @@ public final class WindowPlayer
         FrameLayout container = root.findViewById(R.id.container);
         container.addView(player);
         Context context = player.getContext();
+        displayMetrics = context.getResources().getDisplayMetrics();
         FloatWindow.with(context.getApplicationContext())
                 .setWidth(Screen.width, 0.6f)
                 .setHeight(Screen.height, 0.7f)
                 .setTag(context.getString(R.string.window_key))
-                .setX(Screen.width, 0.6f)
+                .setX(Screen.width, 0.3f)
                 .setY(Screen.height, 0.3f)
+                .setDesktopShow(true)
+                .setMoveType(MoveType.active)
                 .setView(root)
                 .setViewStateListener(this)
                 .build();
         FloatWindow.get(context.getString(R.string.window_key)).show();
+    }
+
+    @Override
+    public void onPositionUpdate(int x, int y) {
+        Resources resources = player.getResources();
+        int width = root.getMeasuredWidth();
+        int height = root.getMeasuredHeight();
+        IFloatWindow floatWindow = FloatWindow.get(resources.getString(R.string.window_key));
+        if (x < 0) {
+            floatWindow.updateX(0);
+        } else if (x + width > displayMetrics.widthPixels) {
+            floatWindow.updateX(displayMetrics.widthPixels - width);
+        }
+        if (y < 0) {
+            floatWindow.updateY(0);
+        } else if (y + height > displayMetrics.heightPixels) {
+            floatWindow.updateY(displayMetrics.heightPixels - height);
+        }
     }
 
     @Override
